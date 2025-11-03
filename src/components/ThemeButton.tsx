09c055f86456721368f25b67e9284ee3142c3c53
Kacy Do <contact@kacydo.dev>
2025-11-03 14:28:49 +1100

    refactor code

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -4,7 +4,7 @@ export default function ThemeButton() {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<form action={() => toggleTheme()}>
+		<form action={toggleTheme}>
 			<p>Current theme is: {theme}</p>
 			<button className='bg-slate-300 dark:bg-slate-800 p-3'>
 				Toggle Theme
--- src/context/ThemeContextProvider.tsx
+++ src/context/ThemeContextProvider.tsx
@@ -8,9 +8,9 @@ export default function ThemeProvider({
 }) {
 	const [theme, setTheme] = useState('light');
 
-	function toggleTheme() {
+	const toggleTheme = () => {
 		setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
-	}
+	};
 
 	return (
 		<ThemeContext.Provider value={{ theme, toggleTheme }}>
